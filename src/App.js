33026cebfa1dfd91adf02fb2b578fcf9b98bc289
mgrybel <monika.grybel@hotmail.com>
2021-03-29 00:42:51 +0100

    Updated the app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
+import LinearGradient from 'react-native-linear-gradient';
+
+export default class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello World!</Text>
+            </View>
+        );
+    }
+}
